23e480c2bfe886f067e6ec8c32cdbaf508b150b2
BIBI <dingdongx2@hanyang.ac.kr>
2018-09-01 15:39:26 +0900

    chapter3.WordTrotter

---
+++ 01.WordTrotter/WordTrotter/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  WordTrotter
+//
+//  Created by 소영 on 01/09/2018.
+//  Copyright © 2018 소영. All rights reserved.
+//
+
+import UIKit
+
+
---
+++ 01.WordTrotter/WordTrotterTests/WordTrotterTests.swift
@@ -0,0 +1,36 @@
+//
+//  WordTrotterTests.swift
+//  WordTrotterTests
+//
+//  Created by 소영 on 01/09/2018.
+//  Copyright © 2018 소영. All rights reserved.
+//
+
+import XCTest
+@testable import WordTrotter
+
+class WordTrotterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
